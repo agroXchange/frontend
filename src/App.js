5f3bafc0bf6d5bc4a03283aa141b3480d7b0cd7d
Scott Baxendale <scottbaxendale@hotmail.co.uk>
2018-04-09 17:12:10 +0200

    Created React App. Installed Dependencies. Implemented React-Router and Redux Store

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
+
+import '../styles/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../styles/index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import {Provider} from 'react-redux'
+import store from './store'
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'));
+  
+registerServiceWorker();
