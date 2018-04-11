cc5421cbd2591e140671575954d0951a3c85d658
toletole412 <yoonjioh90@gmail.com>
2018-04-11 17:32:28 +0200

    Add reducer for fetchOrdersByBuyers

--- src/reducers/orders.js
+++ src/reducers/orders.js
@@ -1,9 +1,13 @@
-import { FETCH_ALL_ORDERS } from "../actions/orders";
+import { FETCH_ALL_ORDERS, FETCH_ORDERS_BY_BUYERID } from "../actions/orders";
 
 export default function(state = [], { type, payload } = {}) {
   switch (type) {
     case FETCH_ALL_ORDERS:
       return payload;
+
+    case FETCH_ORDERS_BY_BUYERID:
+      return payload;
+      
     default:
       return state;
     }
