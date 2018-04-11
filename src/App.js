da339332cde2a768f5fa795e0a9438a043413b86
lucadido06 <lucadidonato06@gmail.com>
2018-04-11 14:14:50 +0200

    users action, add admin landing,pending and transaction components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
+import AdminPage from './components/admin/LandingPage'
+import PendingPage from './components/admin/PendingPage'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import TransactionsPage from './components/admin/TransactionsPage'
 
 import './styles/App.css';
 
 class App extends Component {
   render() {
     return (
+      <MuiThemeProvider>
       <Router>
         <div className="App">
+        <Route exact path="/admin" component={AdminPage} />
+        <Route exact path="/admin/pending" component={PendingPage} />
+        <Route exact path="/admin/transactions" component={TransactionsPage} />
         </div>
       </Router>
+      </MuiThemeProvider>
     );
   }
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,1 +1,5 @@
-export default {}
+import users from './users'
+
+export default {
+  users
+}
