08ba9f68c642f493f158a3e153641e7bce22ebf8
toletole412 <yoonjioh90@gmail.com>
2018-04-12 17:23:39 +0200

    order detail container

---
+++ src/containers/orderList/OrderDetail.js
@@ -0,0 +1,61 @@
+import React, { PureComponent } from 'react'
+import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui/styles';
+import Paper from 'material-ui/Paper'
+import Grid from 'material-ui/Grid';
+import OrderDetailCard from '../../components/orderList/OrderDetailCard'
+import { fetchOrder, changeStatus } from '../../actions/orders'
+
+import { connect } from 'react-redux';
+import compose from "lodash/fp/compose";
+
+import './OrderList.css'
+
+
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing.unit * 2,
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+});
+
+
+
+class OrderDetail extends PureComponent {
+  static propTypes = {
+  classes: PropTypes.object.isRequired
+  };
+
+  componentWillMount() {
+    this.props.fetchOrder(this.props.match.params.id)
+  }
+
+  handleChangeStatus = (data ) => {
+    this.props.changeStatus(data, this.props.match.params.id)
+  }
+
+  render() {
+    const { classes, order } = this.props;
+
+    return (
+      <div className={classes.root}>
+        <OrderDetailCard hello={this.handleChangeStatus} order={order}/>
+      </div>
+    )
+  }
+}
+
+
+const mapStateToProps = (state) => ({
+  order: state.order
+})
+
+export default compose(
+  withStyles(styles),
+  connect(mapStateToProps, { fetchOrder, changeStatus })
+)(OrderDetail);
