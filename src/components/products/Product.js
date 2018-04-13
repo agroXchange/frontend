91857d6319e2a8560d9584300e59c3fa37f8f88e
Billy <droidBilly@users.noreply.github.com>
2018-04-13 17:02:25 +0200

    Update Product.js
    Refactored the imports

--- src/components/products/Product.js
+++ src/components/products/Product.js
@@ -1,22 +1,20 @@
-import React, { PureComponent } from 'react';
+import React, { PureComponent } from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 import compose from 'lodash/fp/compose'
-import PropTypes from 'prop-types';
-
-import { withStyles } from 'material-ui/styles';
-import AppBar from 'material-ui/AppBar';
-import Paper from 'material-ui/Paper';
-import Grid from 'material-ui/Grid';
-import Button from 'material-ui/Button';
-import Typography from 'material-ui/Typography';
+import PropTypes from 'prop-types'
+import { withStyles } from 'material-ui/styles'
+import AppBar from 'material-ui/AppBar'
+import Paper from 'material-ui/Paper'
+import Grid from 'material-ui/Grid'
+import Button from 'material-ui/Button'
+import Typography from 'material-ui/Typography'
 import Dialog, {
   DialogActions,
   DialogContent,
   DialogContentText,
   DialogTitle,
-} from 'material-ui/Dialog';
-
+} from 'material-ui/Dialog'
 import '../../styles/Product.css'
 import { fetchProduct } from '../../actions/products'
 import { createOrder } from '../../actions/orders'
