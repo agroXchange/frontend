c1809b642b468d118c5a67274f9d018416811b6f
Billy <droidBilly@users.noreply.github.com>
2018-04-13 20:52:18 +0200

    Refactored

--- src/components/products/ProductsList.js
+++ src/components/products/ProductsList.js
@@ -1,15 +1,13 @@
 import React, { PureComponent } from 'react'
 import { connect } from 'react-redux'
-import PropTypes from 'prop-types';
-import { withStyles } from 'material-ui/styles';
-import Card, { CardActions, CardContent, CardMedia } from 'material-ui/Card';
-import Button from 'material-ui/Button';
-import Typography from 'material-ui/Typography';
+import PropTypes from 'prop-types'
+import { withStyles } from 'material-ui/styles'
+import Card, { CardActions, CardContent, CardMedia } from 'material-ui/Card'
+import Button from 'material-ui/Button'
+import Typography from 'material-ui/Typography'
 import { Link } from 'react-router-dom'
 import compose from 'lodash/fp/compose'
-import {translate, Trans} from "react-i18next"
-
-
+import {translate, Trans} from 'react-i18next'
 import { fetchAllProducts } from '../../actions/products'
 
 const stockImage = "https://theculinarycook.com/wp-content/uploads/2012/04/vegetable-stock-679x509.jpg"
@@ -32,10 +30,7 @@ const styles = {
   media: {
     height: 100,
   },
-};
-
-
-
+}
 
 class ProductList extends PureComponent {
   static propTypes = {
@@ -118,4 +113,5 @@ const mapStateToProps = (state) => {
 export default compose(
   translate('orders'),
   withStyles(styles),
-  connect(mapStateToProps, { fetchAllProducts }))(ProductList);
+  connect(mapStateToProps, { fetchAllProducts })
+)(ProductList);
