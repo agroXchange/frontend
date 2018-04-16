76f83ac2bdc3abd111eceb9b10ea608f9d99fed3
nsch1 <nick.schooneman1@gmail.com>
2018-04-16 17:44:40 +0200

    Add changes so profileId is used instead of userId where applicable

--- src/actions/users.js
+++ src/actions/users.js
@@ -90,7 +90,7 @@ export const fetchUser = (userId) => (dispatch, getState) => {
   const jwt = state.currentUser.jwt
 
   request
-    .get(`${baseUrl}/users/${userId}`)
+    .get(`${baseUrl}/profiles/${userId}`)
     .set('Authorization', `Bearer ${jwt}`)
     .then(result => {
       dispatch({
