f448244ee325c96589cec932a4a609f87f0da137
nsch1 <nick.schooneman1@gmail.com>
2018-04-16 17:07:20 +0200

    Changed the baseUrl constant to be able to be dynamic for deployment

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,14 @@
 export const localStorageJwtKey = 'currentUserJwt'
 export const localStorageId = 'currentUserId'
+
+let backend
+
+const hostname = window && window.location && window.location.hostname
+
+if (hostname.startsWith('localhost')) {
+  backend = 'http://localhost:4008'
+} else {
+  backend = 'PLACEHOLDER_FOR_DEPLOYED_URL'
+}
+
+export const baseUrl = backend
