4478ec41638a771b99df8634de71d09b4baa8c79
Sanne Stuur <sanne.stuur@gmail.com>
2018-04-17 10:39:21 +0200

    changed sign up to stepped form

--- src/components/signup/SignupPage.js
+++ src/components/signup/SignupPage.js
@@ -1,12 +1,11 @@
 import React, { PureComponent } from "react";
 import { connect } from "react-redux";
 import { signup } from "../../actions/users";
-import SignupForm from "./SignupForm";
+import SteppedSignupForm from "./SteppedSignupForm";
 import { Redirect } from "react-router-dom";
 import compose from "lodash/fp/compose";
 import { translate } from "react-i18next";
 import Typography from "material-ui/Typography";
-import Button from "material-ui/Button";
 import Paper from "material-ui/Paper";
 
 class SignupPage extends PureComponent {
@@ -31,10 +30,8 @@ class SignupPage extends PureComponent {
           {t("Sign up form")}
         </Typography>
         <Typography color="textSecondary">{t("fieldsNecessary")}</Typography>
-        <SignupForm onSubmit={this.handleSubmit} />
+        <SteppedSignupForm onSubmit={this.handleSubmit} />
         <p style={{ color: "red" }}>{this.props.signup.error}</p>
-        <Typography color="textSecondary">{t("alreadyRegistered?")}</Typography>
-        <Button color="primary">{t("Log in")}</Button>
       </Paper>
     );
   }
