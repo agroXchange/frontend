e19107660f038c10055af3822be2b9ea1e384acf
oksmelnik <melnik.oksana@gmail.com>
2018-04-19 09:52:59 +0200

    logo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -158,7 +158,7 @@ class NavBar extends PureComponent {
           <ListItem>
             <img
               style={{ height: "80px", width: "150px" }}
-              src="/images/logo.png"
+              src="/images/logo.jpg"
               alt=""
             />
           </ListItem>
@@ -260,7 +260,7 @@ class NavBar extends PureComponent {
               <ListItem>
                 <img
                   style={{ height: "80px", width: "150px" }}
-                  src="/images/logo.png"
+                  src="/images/logo.jpg"
                   alt=""
                   onClick={() => this.props.history.push("/")}
                 />
