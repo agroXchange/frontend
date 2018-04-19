12a8df4da99b5877552f346c778a333b03483308
nsch1 <nick.schooneman1@gmail.com>
2018-04-19 13:15:15 +0200

    Fix crash after adding product

--- src/reducers/products.js
+++ src/reducers/products.js
@@ -1,17 +1,14 @@
-import { FETCH_ALL_PRODUCTS, FETCH_MY_PRODUCTS, FILTER_PRODUCTS,ADD_PRODUCT } from "../actions/products";
+import { FETCH_ALL_PRODUCTS, FETCH_MY_PRODUCTS, FILTER_PRODUCTS } from "../actions/products";
 
 export default function (state = [], {type, payload}) {
   switch (type) {
-    case ADD_PRODUCT:
-      return [ ...state, payload ]
-  case FETCH_ALL_PRODUCTS:
-    return payload
-  case FETCH_MY_PRODUCTS:
-    return payload
+    case FETCH_ALL_PRODUCTS:
+      return payload
+    case FETCH_MY_PRODUCTS:
+      return payload
     case FILTER_PRODUCTS:
       return payload
-
-  default:
-    return state
+    default:
+      return state
   }
 }
